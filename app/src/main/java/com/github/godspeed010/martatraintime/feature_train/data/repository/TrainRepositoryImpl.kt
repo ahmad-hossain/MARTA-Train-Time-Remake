501240e540a046996b86ab2cfefbdeedb49abe9d
Ahmad Hossain <ahmad.awaab@gmail.com>
2022-03-06 20:19:22 -0500

    added try/catch in TrainRepoImpl to prevent crashing from exceptions

--- app/src/main/java/com/github/godspeed010/martatraintime/feature_train/data/repository/TrainRepositoryImpl.kt
+++ app/src/main/java/com/github/godspeed010/martatraintime/feature_train/data/repository/TrainRepositoryImpl.kt
@@ -1,8 +1,13 @@
 package com.github.godspeed010.martatraintime.feature_train.data.repository
 
+import android.util.Log
 import com.github.godspeed010.martatraintime.feature_train.data.remote.TrainApi
 import com.github.godspeed010.martatraintime.feature_train.domain.model.Train
 import com.github.godspeed010.martatraintime.feature_train.domain.repository.TrainRepository
+import retrofit2.HttpException
+import java.io.IOException
+
+private const val TAG = "TrainRepositoryImpl"
 
 class TrainRepositoryImpl(
     private val api: TrainApi
@@ -10,6 +15,13 @@ class TrainRepositoryImpl(
 
     override suspend fun getTrains(): List<Train> {
         //todo handle IOException and HttpException
-        return api.getTrains()
+        try {
+            return api.getTrains()
+        } catch (e: IOException) {
+            Log.e(TAG, "IOException $e.")
+        } catch (e: HttpException) {
+            Log.e(TAG, "HttpException ${e.code()}")
+        }
+        return emptyList()
     }
 }
