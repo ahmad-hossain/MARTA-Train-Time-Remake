838eb96e4f077ed3972616a8f26eeb51d9c722b4
Name <ahmad.awaab@gmail.com>
2023-01-09 22:33:40 -0500

    Readded Spacer to DefaultRadioButton

--- app/src/main/java/com/github/godspeed010/martatraintime/feature_train/presentation/trains/components/DefaultRadioButton.kt
+++ app/src/main/java/com/github/godspeed010/martatraintime/feature_train/presentation/trains/components/DefaultRadioButton.kt
@@ -1,6 +1,8 @@
 package com.github.godspeed010.martatraintime.feature_train.presentation.trains.components
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
 import androidx.compose.material.RadioButtonDefaults
@@ -8,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun DefaultRadioButton(
@@ -25,6 +28,7 @@ fun DefaultRadioButton(
             text = text,
             style = MaterialTheme.typography.body1
         )
+        Spacer(Modifier.width(8.dp))
         RadioButton(
             selected = selected,
             onClick = onSelect,
